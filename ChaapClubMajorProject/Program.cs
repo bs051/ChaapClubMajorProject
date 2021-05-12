7f0d9bf801e2562b8620ab7a457f446eceea50c0
nk030 <kkaurnavdeep175@gmail.com>
2021-05-12 19:21:09 +1200

    update

--- ChaapClubMajorProject/Program.cs
+++ ChaapClubMajorProject/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace ChaapClubMajorProject
 {
-    public class Program
+    public class Program 
     {
         public static void Main(string[] args)
         {
